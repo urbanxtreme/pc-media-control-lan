2939e11906d1009b8d5cc1c4ef1c4cdfa6cbcc89
urbanxtreme <skrhari2020@gmail.com>
2025-02-22 14:19:31 +0530

    new

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,6 +4,46 @@ function sendCommand(command) {
   socket.emit("media-control", command);
 }
 
+let pendingCommand = null;
+
+function showPasswordDialog(command) {
+  pendingCommand = command;
+  document.getElementById("passwordModal").style.display = "block";
+}
+
+function submitProtectedCommand() {
+  const password = document.getElementById("passwordInput").value;
+  socket.emit("media-control", {
+    command: pendingCommand,
+    password: password,
+  });
+  hidePasswordDialog();
+}
+
+function cancelProtectedCommand() {
+  pendingCommand = null;
+  hidePasswordDialog();
+}
+
+function hidePasswordDialog() {
+  document.getElementById("passwordModal").style.display = "none";
+  document.getElementById("passwordInput").value = "";
+}
+
+// Modify your button click handlers for protected commands
+document
+  .getElementById("shutdownBtn")
+  .addEventListener("click", () => showPasswordDialog("shutdown"));
+document
+  .getElementById("restartBtn")
+  .addEventListener("click", () => showPasswordDialog("restart"));
+document
+  .getElementById("sleepBtn")
+  .addEventListener("click", () => showPasswordDialog("sleep"));
+document
+  .getElementById("lockBtn")
+  .addEventListener("click", () => showPasswordDialog("lock"));
+
 fetch("/ipinfo")
   .then((response) => response.text())
   .then((ip) => {
